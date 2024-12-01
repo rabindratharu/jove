33599d661574a39b029a28e1e2e0ca07ed282b6a
rabindratharu <rabindra.tharu.np@gmail.com>
2024-12-01 17:58:53 +0545

    register new post type as video

--- inc/classes/class-jove.php
+++ inc/classes/class-jove.php
@@ -14,65 +14,108 @@ class Jove {
 	use Singleton;
 
 	/**
-	 * Constructor.
+	 * Class constructor.
+	 *
+	 * The class constructor is responsible for instantiating any necessary
+	 * classes and setting up the WordPress hooks.
 	 *
 	 * @since 1.0.0
 	 */
 	protected function __construct() {
 		// Instantiate necessary classes.
-		Assets::get_instance();
-		Blocks::get_instance();
+		// phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+		$assets = Assets::get_instance();
+		// phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+		$blocks = Blocks::get_instance();
+		// phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+		$post_types = Register_Post_Types::get_instance();
 
-		// Setup WordPress hooks.
+		// Set up WordPress hooks.
 		$this->setup_hooks();
 	}
 
 	/**
 	 * Set up hooks.
 	 *
+	 * This method sets up all the hooks related to the theme,
+	 * such as adding support for core block styles, adding custom
+	 * styles for paginated pages, and enabling SVG uploads.
+	 *
 	 * @since 1.0.0
 	 */
 	protected function setup_hooks() {
 		// Theme setup.
+		// The `after_setup_theme` action hook is used to add support for
+		// core block styles and remove core block patterns.
 		add_action( 'after_setup_theme', [ $this, 'setup_theme' ] );
 
 		// Add custom styles for paginated pages.
+		// The `wp_head` action hook is used to add custom styles for paginated
+		// pages. The styles are added to the `<head>` of the page.
 		add_action( 'wp_head', [ $this, 'add_paginated_styles' ] );
 
 		// Filter for template part areas.
+		// The `default_wp_template_part_areas` filter is used to add areas to
+		// the page that can be used for template parts.
 		add_filter( 'default_wp_template_part_areas', [ $this, 'template_part_areas' ] );
 
 		// Enable SVG uploads.
+		// The `upload_mimes` filter is used to enable SVG uploads.
 		add_filter( 'upload_mimes', [ $this, 'allow_svg_uploads' ] );
+		// The `wp_check_filetype_and_ext` filter is used to sanitize SVG uploads.
 		add_filter( 'wp_check_filetype_and_ext', [ $this, 'sanitize_svg' ], 10, 4 );
 
 		// Adjust SVG display in media library.
+		// The `wp_prepare_attachment_for_js` filter is used to add SVGs to the
+		// media library.
 		add_filter( 'wp_prepare_attachment_for_js', [ $this, 'add_svg_to_media_library' ], 10, 3 );
 	}
 
 	/**
-	 * Theme setup.
+	 * Set up the theme.
+	 *
+	 * This method is responsible for setting up the theme. It adds support
+	 * for core block styles and removes core block patterns.
 	 *
 	 * @since 1.0.0
 	 */
 	public function setup_theme() {
 		// Add support for core block styles.
+		// The `wp-block-styles` feature adds support for block styles.
+		// Block styles are used to customize the look and feel of blocks.
+		// They are defined in the theme.json file.
 		add_theme_support( 'wp-block-styles' );
 
 		// Remove core block patterns.
+		// The `core-block-patterns` feature adds support for block patterns.
+		// Block patterns are pre-designed blocks that can be used to create
+		// content. They are defined in the theme.json file.
+		// We are removing this feature because we are not using block patterns
+		// in this theme.
 		remove_theme_support( 'core-block-patterns' );
 	}
 
 	/**
 	 * Add custom styles to the head for paginated pages.
 	 *
+	 * This method adds custom styles to the head for paginated pages.
+	 * The styles are used to hide the separator between posts on the last
+	 * page of a paginated page.
+	 *
 	 * @since 1.0.0
 	 */
 	public function add_paginated_styles() {
+		// Get the current query.
 		global $wp_query;
 
+		// Check if we are on a paginated page.
 		if ( $wp_query->max_num_pages < 2 ) {
+			// If we are not on a paginated page, add the custom styles.
 			echo '<style>
+				/*
+				 * Hide the separator between posts on the last page of a
+				 * paginated page.
+				 */
 				.blog .wp-block-post-template .wp-block-post:last-child .entry-content + .wp-block-separator,
 				.archive .wp-block-post-template .wp-block-post:last-child .entry-content + .wp-block-separator,
 				.search .wp-block-post-template .wp-block-post:last-child .entry-content + .wp-block-separator {
@@ -85,12 +128,17 @@ class Jove {
 	/**
 	 * Add a sidebar template part area.
 	 *
+	 * This method adds a sidebar template part area to the areas.
+	 * The sidebar template part area is used to add content to the sidebar
+	 * of the page with sidebar template.
+	 *
 	 * @since 1.0.0
 	 *
 	 * @param array $areas Default template part areas.
 	 * @return array Modified areas with sidebar added.
 	 */
 	public function template_part_areas( array $areas ) {
+		// Add sidebar template part area.
 		$areas[] = [
 			'area'        => 'sidebar',
 			'area_tag'    => 'section',
@@ -99,11 +147,15 @@ class Jove {
 			'icon'        => 'sidebar',
 		];
 
+		// Return the modified areas.
 		return $areas;
 	}
 
 	/**
 	 * Allow SVG uploads.
+	 *
+	 * This method adds SVG mime type to the allowed mime types,
+	 * so users can upload SVG files.
 	 *
 	 * @since 1.0.0
 	 *
@@ -111,12 +163,17 @@ class Jove {
 	 * @return array Modified mime types with SVG support.
 	 */
 	public function allow_svg_uploads( $mimes ) {
+		// Add SVG mime type to the allowed mime types.
 		$mimes['svg'] = 'image/svg+xml';
 		return $mimes;
 	}
 
 	/**
 	 * Sanitize SVG uploads.
+	 *
+	 * SVG files uploaded to WordPress are not natively supported. This method
+	 * works around this limitation by modifying the file type data so that
+	 * it is treated as an image.
 	 *
 	 * @since 1.0.0
 	 *
@@ -127,15 +184,21 @@ class Jove {
 	 * @return array Sanitized file data.
 	 */
 	public function sanitize_svg( $data, $file, $filename, $mimes ) {
+		// If the file extension is SVG, set the type to SVG.
 		if ( 'svg' === strtolower( pathinfo( $filename, PATHINFO_EXTENSION ) ) ) {
 			$data['type'] = 'image/svg+xml';
 		}
 
+		// Return the sanitized data.
 		return $data;
 	}
 
 	/**
 	 * Add SVG support to the media library.
+	 *
+	 * This method adds SVG support to the media library by modifying the attachment
+	 * response data. It checks if the attachment is an image and if it is an SVG file.
+	 * If the file is an SVG file, it sets the file path as the image source.
 	 *
 	 * @since 1.0.0
 	 *
@@ -146,17 +209,23 @@ class Jove {
 	 */
 	public function add_svg_to_media_library( $response, $attachment, $meta ) {
 		if ( $response['type'] === 'image' && $response['subtype'] === 'svg+xml' && class_exists( 'SimpleXMLElement' ) ) {
+			// Get the file path of the attachment.
 			$svg_path = get_attached_file( $attachment->ID );
 
+			// Check if the file exists.
 			if ( file_exists( $svg_path ) ) {
+				// Load the SVG file.
 				$svg = simplexml_load_file( $svg_path );
 
+				// Check if the file was loaded successfully.
 				if ( $svg ) {
+					// Set the image source to the file path.
 					$response['image']['src'] = wp_get_attachment_url( $attachment->ID );
 				}
 			}
 		}
 
+		// Return the modified response data.
 		return $response;
 	}
 }
---
+++ inc/classes/class-register-post-types.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Register Post Types
+ *
+ * @package Jove
+ */
+
+namespace Jove\Inc;
+
+use Jove\Inc\Traits\Singleton;
+
+/**
+ * Register Post Types
+ *
+ * @package Jove
+ */
+class Register_Post_Types {
+
+	use Singleton;
+
+	/**
+	 * Private class constructor to prevent direct object creation.
+	 *
+	 * This method sets up all the hooks related to the post type.
+	 * It is meant to be overridden in the classes which implement
+	 * this trait. This is ideal for doing stuff that you only want to
+	 * do once, such as hooking into actions and filters, etc.
+	 */
+	protected function __construct() {
+
+		// load class.
+		$this->setup_hooks();
+	}
+
+	protected function setup_hooks() {
+
+		/**
+		 * Actions.
+		 */
+		add_action( 'init', [ $this, 'create_video_cpt' ], 0 );
+
+	}
+
+	/**
+	 * Register Custom Post Type Video
+	 *
+	 * The video post type is used for storing videos.
+	 *
+	 * @since 1.0.0
+	 */
+	public function create_video_cpt() {
+
+		$labels = [
+			'name'                  => _x( 'Videos', 'Post Type General Name', 'jove' ),
+			'singular_name'         => _x( 'Video', 'Post Type Singular Name', 'jove' ),
+			'menu_name'             => _x( 'Videos', 'Admin Menu text', 'jove' ),
+			'name_admin_bar'        => _x( 'Video', 'Add New on Toolbar', 'jove' ),
+			'archives'              => __( 'Video Archives', 'jove' ),
+			'attributes'            => __( 'Video Attributes', 'jove' ),
+			'parent_item_colon'     => __( 'Parent Video:', 'jove' ),
+			'all_items'             => __( 'All Videos', 'jove' ),
+			'add_new_item'          => __( 'Add New Video', 'jove' ),
+			'add_new'               => __( 'Add New', 'jove' ),
+			'new_item'              => __( 'New Video', 'jove' ),
+			'edit_item'             => __( 'Edit Video', 'jove' ),
+			'update_item'           => __( 'Update Video', 'jove' ),
+			'view_item'             => __( 'View Video', 'jove' ),
+			'view_items'            => __( 'View Videos', 'jove' ),
+			'search_items'          => __( 'Search Video', 'jove' ),
+			'not_found'             => __( 'Not found', 'jove' ),
+			'not_found_in_trash'    => __( 'Not found in Trash', 'jove' ),
+			'featured_image'        => __( 'Featured Image', 'jove' ),
+			'set_featured_image'    => __( 'Set featured image', 'jove' ),
+			'remove_featured_image' => __( 'Remove featured image', 'jove' ),
+			'use_featured_image'    => __( 'Use as featured image', 'jove' ),
+			'insert_into_item'      => __( 'Insert into Video', 'jove' ),
+			'uploaded_to_this_item' => __( 'Uploaded to this Video', 'jove' ),
+			'items_list'            => __( 'Videos list', 'jove' ),
+			'items_list_navigation' => __( 'Videos list navigation', 'jove' ),
+			'filter_items_list'     => __( 'Filter Videos list', 'jove' ),
+		];
+		$args   = [
+			'label'               => __( 'Video', 'jove' ),
+			'description'         => __( 'The videos', 'jove' ),
+			'labels'              => $labels,
+			'menu_icon'           => 'dashicons-video-alt',
+			'supports'            => [
+				'title',
+				'editor',
+				'excerpt',
+				'thumbnail',
+				'revisions',
+				'author',
+				'comments',
+				'trackbacks',
+				'page-attributes',
+				'custom-fields',
+			],
+			'taxonomies'          => [],
+			'public'              => true,
+			'show_ui'             => true,
+			'show_in_menu'        => true,
+			'menu_position'       => 5,
+			'show_in_admin_bar'   => true,
+			'show_in_nav_menus'   => true,
+			'can_export'          => true,
+			'has_archive'         => true,
+			'hierarchical'        => false,
+			'exclude_from_search' => false,
+			'show_in_rest'        => true,
+			'publicly_queryable'  => true,
+			'capability_type'     => 'post',
+		];
+
+		/**
+		 * Register the post type.
+		 *
+		 * @since 1.0.0
+		 */
+		register_post_type( 'videos', $args );
+
+	}
+}
