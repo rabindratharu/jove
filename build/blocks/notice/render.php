45afde9ba501da2087fb184e208bfda46356fa51
rabindratharu <rabindra.tharu.np@gmail.com>
2024-12-28 08:23:51 +0545

    fixes

--- build/blocks/notice/render.php
+++ build/blocks/notice/render.php
@@ -54,10 +54,21 @@ $inner_blocks_template = [
      <?php } ?>
 
      <div class="jove-notice-block">
-         <div class="jove-notice-block__icon">Icon</div>
+         <div class="jove-notice-block__icon"><svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                 xmlns="http://www.w3.org/2000/svg">
+                 <path
+                     d="M12 22C6.477 22 2 17.523 2 12C2 6.477 6.477 2 12 2C17.523 2 22 6.477 22 12C22 17.523 17.523 22 12 22ZM11 11V17H13V11H11ZM11 7V9H13V7H11Z"
+                     fill="#2183ED" />
+             </svg>
+         </div>
          <InnerBlocks class="jove-notice-block__innerblocks" orientation="horizontal"
              template="<?php echo esc_attr( wp_json_encode( $inner_blocks_template ) ); ?>" />
-         <div class="jove-notice-block__close">Close</div>
+         <div class="jove-notice-block__close"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                 viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                 stroke-linejoin="round" class="feather feather-x">
+                 <line x1="18" y1="6" x2="6" y2="18"></line>
+                 <line x1="6" y1="6" x2="18" y2="18"></line>
+             </svg></div>
 
      </div>
      <?php if ( ! $is_preview ) { ?>
